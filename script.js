a1bf4412e96d295728cd7f073df8b8d4efa18f54
zaz7 <abdullah75farid@gmail.com>
2024-07-19 21:07:32 +0300

    js56

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     let gameActive = true;
 
     const whenToWin = [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    
 
     function cellClick(e){
         const cell =e.target;
@@ -31,7 +32,13 @@ document.addEventListener("DOMContentLoaded",()=>{
         for(let i = 0; i<whenToWin.length; i++
         ){
             const winCond = whenToWin[i];
-            const a = winCond[0]
+            
+            const a = cells[winCond[0]].textContent;//Since winCond contains indices of cells that need to be checked for a win condition, cells[winCond[0]] accesses the cell at that index. The .textContent property gets or sets the text content of the specified element, which in this case, would be either "X", "O", or an empty string if the cell is not filled.
+            const b = cells[winCond[1]];
+            const c = cells[winCond[2]];
+
+            if()
+
 
         }
     }
