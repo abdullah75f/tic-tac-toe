3a851075db04ef9817eb9bf1b403caf5d12078e6
zaz7 <abdullah75farid@gmail.com>
2024-07-19 18:57:33 +0300

    winchecker

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
 document.addEventListener("DOMContentLoaded",()=>{
     const cells = document.querySelectorAll(".cell");
     let currenPlayer = "X";
     const playerTurnIndicator = document.querySelector(".player-turn");
-})
+    const score = document.querySelector("score");
+    const winnerIndicator =document.querySelector(".winner-indicator");
+    let gameActive = true;
+
+    const whenToWin = [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+});
