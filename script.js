a1ecf0d7fa14fe9009ed52a725bb39bb7e29d298
zaz7 <abdullah75farid@gmail.com>
2024-07-19 19:15:59 +0300

    js3

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 
 document.addEventListener("DOMContentLoaded",()=>{
     const cells = document.querySelectorAll(".cell");
-    let currenPlayer = "X";
+    let currentPlayer = "X";
     const playerTurnIndicator = document.querySelector(".player-turn");
     const score = document.querySelector("score");
     const winnerIndicator =document.querySelector(".winner-indicator");
     let gameActive = true;
 
     const whenToWin = [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    function cellClick(e){
+        const cell =e.target;
+        const cellIndex = Array.from(cells).indexOf(cell);
+
+        if(cell.textContent.trim() !== "" || !gameActive){
+            return ; 
+
+        }
+        cell.textContent = currentPlayer;
+    }
+
+    // cells.forEach(cell => {
+    //     cell.addEventListener('click', cellClick);
+    // });
+    
 });
+
